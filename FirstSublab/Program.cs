97ba8450caf50ba0015496db3f774f8a5c2987e0
smoothbronx <vlad.uhwatov@yandex.ru>
2022-12-08 21:34:42 +0200

    Lab 11.1 done

---
+++ FirstSublab/Program.cs
@@ -0,0 +1,17 @@
+﻿using FirstSublab;
+
+Console.Write("Vector start Coords 'X, Y': ");
+var startCoords = Console.ReadLine()!.Split(",").Select(int.Parse).ToArray();
+
+Console.Write("Vector end Coords 'X, Y': ");
+var endCoords = Console.ReadLine()!.Split(",").Select(int.Parse).ToArray();
+Console.WriteLine();
+
+var userVector = new Vector(
+    new[] { startCoords[0], startCoords[1] },
+    new[] { endCoords[0], endCoords[1] });
+
+
+var userVectorMiddleCoords = userVector.GetCoordsOfVectorMiddle();
+Console.WriteLine($"Coords of vector middle: ({userVectorMiddleCoords[0]}, {userVectorMiddleCoords[1]})");
+Console.WriteLine($"Vector corner is{(userVector.CornerIs45deg() ? " " : " not")} equal 45deg");
---
+++ FirstSublab/Vector.cs
@@ -0,0 +1,54 @@
+﻿namespace FirstSublab;
+
+public class Vector
+{
+    private int startXCoord;
+    private int startYCoord;
+    private int endXCoord;
+    private int endYCoord;
+
+    public Vector(int startXCoord, int startYCoord, int endXCoord, int endYCoord)
+    {
+        this.startXCoord = startXCoord;
+        this.startYCoord = startYCoord;
+        this.endXCoord = endXCoord;
+        this.endYCoord = endYCoord;
+    }
+
+    public Vector(int[] startCoords, int[] endCoords)
+    {
+        if (startCoords.Length != 2) throw new Exception("startCoords must contain two coordinates");
+        if (endCoords.Length != 2) throw new Exception("endCoords must contain two coordinates");
+
+        startXCoord = startCoords[0];
+        startYCoord = startCoords[1];
+        endXCoord = endCoords[0];
+        endYCoord = endCoords[1];
+    }
+
+    public void SetStartCoords(int xCoord, int yCoord)
+    {
+        startXCoord = xCoord;
+        startYCoord = yCoord;
+    }
+    
+    public void SetEndCoords(int xCoord, int yCoord)
+    {
+        endXCoord = xCoord;
+        endYCoord = yCoord;
+    }
+
+    public double[] GetCoordsOfVectorMiddle()
+    {
+        return new[]
+        {
+            (double) (startXCoord + endXCoord) / 2,
+            (double) (startYCoord + endYCoord) / 2,
+        };
+    }
+
+    public bool CornerIs45deg()
+    {
+        return Math.Abs(endXCoord - startXCoord) - Math.Abs(endYCoord - startYCoord) == 0;
+    }
+}
